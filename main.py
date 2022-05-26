fc47a24cdd33c18e90875aab2330b8d431bad948
Jayden Clark <jayden260100@gmail.com>
2022-05-27 10:31:48 +1200

    Window switching

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class GameManager():
         self.root.title("EzSoil Game")
         self.root.geometry("1000x600")
         self.root.resizable(False, False)
+        self.frame = tkinter.Frame(self.root)
+        self.frame.pack(side="top", expand=True, fill="both")
         windowManager(self)
         self.main_loop()
         
@@ -15,15 +17,45 @@ class GameManager():
         self.root.mainloop()
 
 # Window sub class
+class bathroom():
+    def __init__(self, window_manager):
+        self.window_manager = window_manager
+        self.root = window_manager.game_manager.frame
+
+    def show(self):
+        self.window_manager.clear()
+        self.root.configure(background='white')
+        tkinter.Button(self.root, text ="Hello", command = self.button).pack()
+
+    def button(self):
+        self.window_manager.menu.show()
+
+# Window sub class
 class menu():
     def __init__(self, window_manager):
-        window_manager.game_manager.root.configure(background='black')
+        self.window_manager = window_manager
+        self.root = window_manager.game_manager.frame
+
+    def show(self):
+        self.window_manager.clear()
+        self.root.configure(background='white')
+        tkinter.Button(self.root, text ="Bathroom", command = self.button).pack()
+
+    def button(self):
+        self.window_manager.bathroom.show()
 
 # Window manager
 class windowManager():
     def __init__(self, game_manager):
         self.game_manager = game_manager
         self.menu = menu(self)
+        self.bathroom = bathroom(self)
+
+        self.menu.show()
+
+    def clear(self):
+        for widgets in self.game_manager.frame.winfo_children():
+            widgets.destroy()
         
 if __name__ == '__main__':
     GameManager()
