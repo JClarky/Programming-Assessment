524c18aeb4853aa0cb4499d1064012234e98bf95
Jayden Clark <jayden260100@gmail.com>
2022-06-03 08:44:58 +1200

    master

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ class GameManager():
         self.root.resizable(False, False)
         self.frame = Frame(self.root)
         self.frame.pack(side="top", expand=True, fill="both")
-        frameManager(self)
+        frame_manager = frameManager(self)
+        plantManager().spawn(frame_manager.bathroom)
+        print("TICKERRR NEEDS TO BE HAPPENING ONCE A SECOND")
         self.main_loop()
-        
+       
     def main_loop(self):
         self.root.mainloop()
 
@@ -35,21 +37,35 @@ class frameManager():
     def clear(self):
         for widgets in self.game_manager.frame.winfo_children():
             widgets.destroy()
+            
+class plantManager():
+    def __init__(self):
+        print("ahh yes")
+    def spawn(self, environment=None):
+        print("spawning plant in random location, unless otherwise requested")
 
 #
 # Manager subclasses
 #
+
+class plant():
+    def __init__(self, environment):
+        print("choosing type of plant (from database) and spawning at requested environment")
+        
+    def update(self):
+        print("depending on the time elaspsed, update the plant conditiosns accordingly")
 
 # Frame sub class
 class bathroom():
     def __init__(self, frame_manager):
         self.frame_manager = frame_manager
         self.frame = frame_manager.game_manager.frame
+        self.plants = []
 
     def show(self):
         self.frame_manager.clear()
         self.frame.configure(background='white')
-        self.frame_manager.game_manager.root.title("bathroom")
+        self.frame_manager.game_manager.root.title("Bathroom")
         canvas = Canvas(self.frame, width=self.frame_manager.game_manager.width, height=self.frame_manager.game_manager.height)
         canvas.pack()
 
@@ -57,10 +73,6 @@ class bathroom():
         canvas.background = img  # Keep a reference in case this code is put in a function.
         bg = canvas.create_image(0, 0, anchor=NW, image=img)
         navbar(self, self.frame)
-
-        
-
-
 
 # Frame sub class
 class menu():
@@ -85,7 +97,7 @@ class menu():
         label.pack()
 
         Button(container, text="Play", command=lambda: self.frame_manager.bathroom.show()).pack()
-        Button(container, text="Instructions", command=lambda: print("show intructions")).pack()
+        Button(container, text="Instructions", command=lambda: plantManager()).pack()
         Button(container, text="Exit", command=lambda: self.frame_manager.game_manager.root.destroy()).pack()
 
         copyright = Label(self.frame, text="Copyright EzSoil 2022", background="white")
@@ -104,7 +116,7 @@ class navbar():
         container = Frame(self.frame, background = "white")
         container.place(anchor="n")
 
-        Button(self.frame, text ="Bathroom", command = self.bathroom_button).pack()
+        Button(self.frame, text ="Bathroom", command = self.bathroom_button).place(anchor="e", x=self.parent_frame.frame_manager.game_manager.width/2, y=15)
         Button(self.frame, text ="Menu", command = self.menu_button).place(anchor="e", x=self.parent_frame.frame_manager.game_manager.width, y=15)
 
     def bathroom_button(self):
@@ -113,6 +125,5 @@ class navbar():
     def menu_button(self):
         self.parent_frame.frame_manager.menu.show()
 
-        
 if __name__ == '__main__':
     GameManager()
